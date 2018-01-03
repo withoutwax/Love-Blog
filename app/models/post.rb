7eb3ea72b57252b6068f049516fb88303d7eeab6
Will Kim <rlagmlckd@gmail.com>
2018-01-03 03:19:22 -0600

    Eliminated Posts prone to error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: { minimum: 5 }
+  validates :title, presence: true
   validates :body, presence: true
 end
